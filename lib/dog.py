e78fe06c0cee34bbb67c13e3303a1de1a4936433
leon <onditileon21@gmail.com>
2024-03-04 12:22:17 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,31 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Mastiff"):
+        self.name = name
+        self.breed = breed
+    
+    def set_name(self, name):
+        if isinstance(name,str) and 1 <= len(name) <=25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+        
+    def get_name(self):
+        return self._name
+    
+    def set_breed(self, breed):
+        if breed.title() in APPROVED_BREEDS:
+            self._breed = breed.title()
+            return self._breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    def get_breed(self):
+        return self._breed
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
+nino = Dog("", "")
+
+
+  
--- lib/person.py
+++ lib/person.py
@@ -4,7 +4,7 @@ APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
-    "ITC",
+    "Itc",
     "Production",
     "Legal",
     "Finance",
@@ -16,4 +16,43 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Guido", job="Marketing"):
+        # self.name = None
+        # self.job = None
+        self.name = name
+        self.job = job 
+        # if job in APPROVED_JOBS else print("Job must be in list of approved jobs.")
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_name(self):
+        return self._name
+
+        
+    def set_job(self, job):
+        
+        # return "Hereeee"
+        # print(job)
+        if job.title() in APPROVED_JOBS: 
+            self._job = job
+            # return "Persons job added"
+        else:
+            # self._job =None
+            print ("Job must be in list of approved jobs.")
+
+    def get_job(self):
+        return self._job
+
+    
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
+# Wafula = Person("", "")
+person2 = Person(job="Benevolent dictator for life")
+    
+
+
